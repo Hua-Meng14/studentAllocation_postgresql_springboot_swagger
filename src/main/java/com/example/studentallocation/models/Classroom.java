34123b97864419351c1aa8e4efc3a8a99a9d5e6d
Hua-Meng14 <lim.huameng19@kit.edu.kh>
2023-05-27 23:51:24 +0700

    add classroom model

---
+++ src/main/java/com/example/studentallocation/models/Classroom.java
@@ -0,0 +1,52 @@
+package com.example.studentallocation.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "classrooms")
+public class Classroom {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private int capacity;
+
+    // Constructor
+    public Classroom() {
+
+    }
+
+    public Classroom(String name, int capacity) {
+        this.name = name;
+        this.capacity = capacity;
+    }
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+}
