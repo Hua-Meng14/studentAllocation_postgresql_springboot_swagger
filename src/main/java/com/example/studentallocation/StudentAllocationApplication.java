a33566d4712208ad5698dc0e22b2da434abd83fc
Hua-Meng14 <lim.huameng19@kit.edu.kh>
2023-05-27 23:26:59 +0700

    initialize project

---
+++ src/main/java/com/example/studentallocation/StudentAllocationApplication.java
@@ -0,0 +1,13 @@
+package com.example.studentallocation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentAllocationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentAllocationApplication.class, args);
+	}
+
+}
